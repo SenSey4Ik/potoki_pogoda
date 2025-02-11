fa0df6d57a3812a004763fd3f759948987bf5b3c
SenSey4Ik <massageinekb20@mail.ru>
2025-02-11 22:48:07 +0700

    Версия с 1 функцией и 8 потоками

--- potoki_pogoda.py
+++ potoki_pogoda.py
@@ -37,10 +37,9 @@ def mnogopotok_pog(argyment):
         print(f'В Бийске{data} градусов')
 
 
-def potok_1():
+def potok_1(kol_pot, argyment):
     stat_time = time.time()
-    argyment = 24
-    for i in range(1):
+    for i in range(kol_pot):
         threads = [Thread(target=mnogopotok_pog(argyment))]
 
     for t in threads:
@@ -50,128 +49,20 @@ def potok_1():
         t.join()
 
     end_time = time.time() - stat_time
-    print(f'Время выполнения программы с одним потоком {end_time}')
-
-
-def potok_2():
-    stat_time = time.time()
-    argyment = 12
-    for i in range(2):
-        threads = [Thread(target=mnogopotok_pog(argyment))]
-
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    end_time = time.time() - stat_time
-    print(f'Время выполнения программы с двумя потоками{end_time}')
-
-def potok_3():
-    stat_time = time.time()
-    argyment = 8
-    for i in range(3):
-        threads = [Thread(target=mnogopotok_pog(argyment))]
-
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    end_time = time.time() - stat_time
-    print(f'Время выполнения программы с тремя потоками{end_time}')
-
-def potok_4():
-    stat_time = time.time()
-    argyment = 6
-    for i in range(4):
-        threads = [Thread(target=mnogopotok_pog(argyment))]
-
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    end_time = time.time() - stat_time
-    print(f'Время выполнения программы с четыремя потоками{end_time}')
-
-def potok_5():
-    stat_time = time.time()
-    argyment = 5
-    for i in range(5):
-        threads = [Thread(target=mnogopotok_pog(argyment))]
-
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    end_time = time.time() - stat_time
-    print(f'Время выполнения программы с пятью потоками {end_time}')
-
-def potok_6():
-    stat_time = time.time()
-    argyment = 4
-    for i in range(6):
-        threads = [Thread(target=mnogopotok_pog(argyment))]
-
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    end_time = time.time() - stat_time
-    print(f'Время выполнения программы с шестью потоками{end_time}')
-
-def potok_7():
-    stat_time = time.time()
-    argyment = 3
-    for i in range(7):
-        threads = [Thread(target=mnogopotok_pog(argyment))]
-
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    end_time = time.time() - stat_time
-    print(f'Время выполнения программы с семью потоками{end_time}')
-
-def potok_8():
-    stat_time = time.time()
-    argyment = 3
-    for i in range(8):
-        threads = [Thread(target=mnogopotok_pog(argyment))]
-
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    end_time = time.time() - stat_time
-    print(f'Время выполнения программы с восьмью потоками{end_time}')
-
-
+    print(f'Время выполнения программы с {kol_pot} потоком {end_time}')
 
 
 
 
 def main():
-    potok_1()
-    potok_2()
-    potok_3()
-    potok_4()
-    potok_5()
-    potok_6()
-    potok_7()
-    potok_8()
+    potok_1(1,24)
+    potok_1(2,12)
+    potok_1(3,8)
+    potok_1(4,6)
+    potok_1(5,5)
+    potok_1(6,4)
+    potok_1(7,3)
+    potok_1(8,3)
 
 
 main()
